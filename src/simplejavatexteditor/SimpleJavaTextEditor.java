5f665c05ebe06d4ee3aa45e7bb0c2190d3e2f89b
eslam <eslam.faisal.ef@gmail.com>
2019-12-20 23:11:24 +0200

    author

--- src/simplejavatexteditor/SimpleJavaTextEditor.java
+++ src/simplejavatexteditor/SimpleJavaTextEditor.java
@@ -6,9 +6,9 @@ import javax.swing.JTextPane;
 public class SimpleJavaTextEditor extends JTextPane {
 
     private static final long serialVersionUID = 1L;
-    public final static String AUTHOR_EMAIL = "hi@ph7.me";
-    public final static String NAME = "PHNotePad";
-        public final static String EDITOR_EMAIL = "contact@achinthagunasekara.com";
+    public final static String AUTHOR_EMAIL = "Mostafahelala78@gmail.com";
+    public final static String NAME = "Mostafa Khalid Abd Elsalam";
+        public final static String EDITOR_EMAIL = "Mostafahelala78@gmail.com";
     public final static double VERSION = 3.0;
 
     /**
